bca6dfa7ed0b09cc7a3038a577c8bde198b8c51e
simonebatista23 <simoneliveira1224@gmail.com>
2025-10-13 02:04:08 -0300

    ajustando tela e adicionado sessoes

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,97 @@ const toggleBtn = document.getElementById("mobile-toggle");
 const mobileMenu = document.getElementById("mobile-menu");
 
 toggleBtn.addEventListener("click", () => {
+    toggleBtn.classList.toggle("active");document.addEventListener('DOMContentLoaded', function () {
+
+    const featureCards = document.querySelectorAll('.feature-card');
+
+    featureCards.forEach(card => {
+
+        const iconButton = card.querySelector('.icon-button');
+
+ 
+        const allTargetTexts = card.querySelectorAll('.p1, .p2, .p3');
+
+
+        if (iconButton && allTargetTexts.length > 0) {
+
+           
+            const handleMouseOver = function () {
+                allTargetTexts.forEach(p => p.classList.add('show-text'));
+            };
+
+            const handleMouseOut = function () {
+                allTargetTexts.forEach(p => p.classList.remove('show-text'));
+            };
+
+           
+            iconButton.addEventListener('mouseover', handleMouseOver);
+            iconButton.addEventListener('mouseout', handleMouseOut);
+        }
+    });
+
+  
+});
+
+const toggleBtn = document.getElementById("mobile-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+toggleBtn.addEventListener("click", () => {
     toggleBtn.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+});
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    const sectionsToAnimate = document.querySelectorAll('[data-animate-on-scroll]');
+
+    // Define as opções do observador
+    const observerOptions = {
+        root: null, 
+        rootMargin: '0px',
+
+        threshold: 0.15
+    };
+
+ 
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+   
+            if (entry.isIntersecting) {
+
+                entry.target.classList.remove('opacity-0', 'translate-y-12');
+
+
+                observer.unobserve(entry.target);
+            }
+        });
+    };
+
+    // Cria e inicia a observação
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    sectionsToAnimate.forEach(section => {
+        observer.observe(section);
+    });
+
+
+    const scrollLink = document.getElementById('cta-areas-atuacao-link');
+    const targetId = 'areas-de-atuacao';
+    const targetSection = document.getElementById(targetId);
+
+    if (scrollLink && targetSection) {
+        scrollLink.addEventListener('click', function (e) {
+            e.preventDefault(); 
+
+            targetSection.scrollIntoView({
+                behavior: 'smooth' 
+            });
+        });
+    }
+
+});
+
+
     mobileMenu.classList.toggle("active");
 });
 
